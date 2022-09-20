bda775b978bfb0ffc52527315e1b22333114a174
gwebb17 <gwebb1723@gmail.com>
2022-09-20 09:26:56 -0700

    edited with animation

--- lms_assignment1/wwwroot/js/site.js
+++ lms_assignment1/wwwroot/js/site.js
@@ -5,3 +5,21 @@
 function changeColor() {
     document.getElementById("heading_id").style.color = "red";
 }
+
+
+var id = null;
+function myMove() {
+    var elem = document.getElementById("animation_divA");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
